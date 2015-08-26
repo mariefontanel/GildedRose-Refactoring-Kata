d93341a3956532b2f6b45cb2a3c29bb3749949f1
marieh <marieherelier@gmail.com>
2015-08-26 11:46:03 +0200

    refactor: inverser les if/else

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,56 +6,56 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            if item.name != "Conjured Mana Cake"
-              item.quality = item.quality - 1
-            else
-              item.quality = item.quality - 2
-            end
-          end
-        end
-      else
+      if item.name == "Aged Brie"
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+        end
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+        if item.sell_in < 11 and 
+          if item.quality < 50
+            item.quality = item.quality + 1
           end
         end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      elsif item.name == "Elixir of the Mongoose"
+        if item.quality > 0
+          item.quality = item.quality - 1
+        end
+      elsif item.name == "Conjured Mana Cake"
+        if item.quality > 0
+          item.quality = item.quality - 2
+        end
+      else
+        item.quality = 80
       end
+      
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+      
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.name != "Conjured Mana Cake"
-              if item.quality > 0
-                if item.name != "Sulfuras, Hand of Ragnaros"
-                  item.quality = item.quality - 1
-                end
-              end
-            else
-              if item.quality > 0
-                item.quality = item.quality - 2
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
+        if item.name == "Aged Brie"
           if item.quality < 50
             item.quality = item.quality + 1
+          end
+        elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+          item.quality = 0       
+        elsif item.name == "Sulfuras, Hand of Ragnaros"
+          item.quality = 80
+        elsif item.name == "Conjured Mana Cake"
+          if item.quality > 0
+            item.quality = item.quality - 2
+          end
+        else
+          if item.quality > 0
+            item.quality = item.quality - 1
           end
         end
       end
